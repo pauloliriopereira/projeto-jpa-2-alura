12ed502bbe6c7a27240e730196ddda144ed4cb0d
paulolirio <paulolirio@n46v>
2018-08-28 22:54:18 -0300

    Habilitando cache de segundo nível.

--- projeto-jpa-2/src/main/java/br/com/caelum/model/Produto.java
+++ projeto-jpa-2/src/main/java/br/com/caelum/model/Produto.java
@@ -15,9 +15,12 @@ import javax.persistence.Version;
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
 
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
+@Cache(usage=CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 public class Produto {
 
 	@Id
@@ -36,10 +39,12 @@ public class Produto {
 	private double preco;
 	
 	@ManyToMany
+	@Cache(usage=CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 	private List<Categoria> categorias = new ArrayList<Categoria>();
 	
 	@Valid
 	@ManyToOne
+	@Cache(usage=CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 	private Loja loja;
 	
 	@Version
